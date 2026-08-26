8c73e240a65b0036a548fefc74be6742dade5ab7
MiqNVISION <miquel.alfaras@nvision.es>
2026-08-26 10:12:14 +0200

    Recoded as class MainWindow

--- acqstat.py
+++ acqstat.py
@@ -2,18 +2,28 @@ import sys # Needed for argv
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
 
 
+# MainWindow class 
+class MainWindow(QWidget):
+    
+    # Initialise class and QWidget
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("acqstat")
+        self.resize(1000, 700)
+
+        layout = QVBoxLayout()
+
+        label = QLabel("Drop CSV here")
+        layout.addWidget(label)
+
+        self.setLayout(layout)
+
+
 app = QApplication(sys.argv)
 
-window = QWidget()
-window.setWindowTitle("acqstat")
-window.resize(1000, 700)
-
-layout = QVBoxLayout()
-
-label = QLabel("Drop CSV here")
-layout.addWidget(label)
-
-window.setLayout(layout)
+# Instantiate
+window = MainWindow()
 
 window.show()
 
